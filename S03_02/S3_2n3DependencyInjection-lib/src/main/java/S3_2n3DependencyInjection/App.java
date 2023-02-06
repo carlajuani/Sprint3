1369a54aa9e4084983d4dbbbbd92adc54e4f3bc9
carlajuani <carla90_@hotmail.es>
2023-02-06 19:59:05 +0100

    prova

--- S03_02/S3_2n3DependencyInjection-lib/src/main/java/S3_2n3DependencyInjection/App.java
+++ S03_02/S3_2n3DependencyInjection-lib/src/main/java/S3_2n3DependencyInjection/App.java
@@ -7,7 +7,7 @@ package S3_2n3DependencyInjection;
 public class App {
 
 	public static void main(String[] args) {
-		//emprem els metodes estatics del currencyConvertor per fer el canvi de moneda del preu de l'article
+		//emprem els metodes estatics del currencyConvertor per fer el canvi de moneda del preu de l'article 
 		CurrencyConvertor.convertToPounds(new Article("lollipop", 1.95, "€"));
 		CurrencyConvertor.convertToEuros(new Article("bicycle", 325.00, "$"));
 		CurrencyConvertor.convertToPounds(new Article("cookies", 6.30, "$"));
