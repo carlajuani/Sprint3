953c928f8b1dccd9474fcf35b02d434c985733b3
carlajuani <carla90_@hotmail.es>
2023-02-14 09:43:33 +0100

    correccio Singleton

--- S03_01/src/main/java/n1singleton/App.java
+++ S03_01/src/main/java/n1singleton/App.java
@@ -8,7 +8,7 @@ public class App {
 	    do
 	    {
 	      //Instanciem l'atribut ArrayList d'Strings a través del mètode estàtic de la classe Undo getInstance
-	      Undo.setInstance();
+	      Undo undo = Undo.setInstance();
 	      System.out.println("\n1.Insert new command");
 	      System.out.println("2.Undo last command");
 	      System.out.println("3.See command's list");
@@ -17,15 +17,15 @@ public class App {
 	      switch(selectedOption)
 	      {
 	       case 1: //Amb el mètode estàtic takeCommand d'Undo demanem el command per guardar com a String al commandsList
-	    	   Undo.takeCommand(Input.scanningForString("Please insert command"));
+	    	   undo.takeCommand(Input.scanningForString("Please insert command"));
 	    	   break;
 	    	   
 	       case 2: //Amb undoCommand esborreml'últim command afegit
-	    	   Undo.undoCommand();
+	    	   undo.undoCommand();
 	    	   break;
 	    	   
 	       case 3: //Amb showCommands imprimim forEach element de l'ArrayList de commands
-	    	   Undo.showCommands();
+	    	   undo.showCommands();
 	    	   break;
 
 	       case 4:
--- S03_01/src/main/java/n1singleton/Undo.java
+++ S03_01/src/main/java/n1singleton/Undo.java
@@ -4,23 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Undo {
-	private static List<String> commandsList;
-	//singleton object
-	public static void setInstance() {
-		if (commandsList == null) {
-			commandsList = new ArrayList<String>();
+	private static Undo instance;
+	private List<String> commandsList;
+	
+	private Undo(List<String> commandsList) {
+        this.commandsList = commandsList;
+	}
+	
+	public static Undo setInstance() {
+		if (instance == null) {
+			instance = new Undo(new ArrayList<String>());
 		}
+		return instance;
 	}
 	
-	public static void takeCommand(String command) {
-		commandsList.add(command);
+	public void takeCommand(String command) {
+		this.commandsList.add(command);
 	}
 	
-	public static void undoCommand() {
-		commandsList.remove(commandsList.size()-1);
+	public void undoCommand() {
+		this.commandsList.remove(this.commandsList.size()-1);
 	}
 	
-	public static void showCommands() {
+	public void showCommands() {
 		commandsList.stream().forEach(System.out::println);
 	}
 }
